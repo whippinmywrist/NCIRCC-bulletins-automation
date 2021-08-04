4c6f4a24888d9f45e8843dc037db580027ea4728
Антон Скворцов <azicazic@yandex.ru>
2021-08-04 17:42:02 +0300

    update

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
+from jinja2 import Template
+import smtplib
+from email.message import EmailMessage
+import ssl
 import time
-import camelot
+
+template = """
+        <dl class="inline">
+            <dt>Summary</dt><dd>{{ a.title }}</dd>
+            <dt>Дата публикации</dt><dd>{{ a.publication_date }}</dd>
+            <dt>Дата выявления</dt><dd>{{ a.detection_date }}</dd>
+            <dt>Производитель ПО</dt><dd>{{ a.vendor }}</dd>
+            <dt>Наименование ПО</dt><dd>{{ a.products }}</dd>
+            <dt>Уровень опасности</dt><dd>{{ a.severity }}</dd>
+            <dt>CVSSv3 Score</dt><dd>{{ a.cvss_v3_score }}</dd>
+            <dt>Links</dt><dd>{{ bulletin_pdf_url }}</dd>
+        </dl>"""
+
 
 def get_bulletins(page=1):
     r = requests.get("https://safe-surf.ru/specialists/bulletins-nkcki/?PAGEN_1={}".format(page))
@@ -11,42 +27,59 @@ def get_bulletins(page=1):
     for vuln in soup.find_all("div", "blockBase blockBulletine"):
         title = vuln.find('h4').text.strip()
         bulletin_pdf_url = "https://safe-surf.ru{}".format(vuln.find('h4').find('a')['href'])
+        result.update(
+            {
+                title: {
+                    'title': title,
+                    'bulletin_pdf_url': bulletin_pdf_url
+                }
+            })
         for line in vuln.find_all('div', 'stringBase clearfix'):
-            #print(line.text)
-            print('---------')
+            # print(line.text)
             if 'Дата:' in line.text:
                 publication_date = " ".join(line.text.replace('Дата:', '').split())
+                result[title]['publication_date'] = publication_date
             if 'Дата выявления:' in line.text:
                 detection_date = " ".join(line.text.replace('Дата выявления:', '').split())
+                result[title]['detection_date'] = detection_date
             if 'Производитель ПО:' in line.text:
                 vendor = " ".join(line.text.replace('Производитель ПО:', '').split())
+                result[title]['vendor'] = vendor
             if 'Наименование ПО:' in line.text:
                 products = []
                 for product in line.text.replace('Наименование ПО:', '').split('\n\n\n\n'):
                     p = " ".join(product.split())
                     if p != '':
                         products.append(p)
+                result[title]['products'] = products
             if 'Уровень опасности:' in line.text:
                 a = " ".join(line.text.replace('Уровень опасности:', '').split())
                 severity = a.split(' ')[0]
                 cvss_v3_score = a.split(' ')[1][1:-1]
-        result.update(
-            {
-                title: {
-                    'Заголовок': title,
-                    'Ссылка на PDF': bulletin_pdf_url,
-                    'Дата публикации': publication_date,
-                    'Дата выявления': detection_date,
-                    'Производитель ПО': vendor,
-                    'Наименование ПО': products,
-                    'Уровень опасности': severity,
-                    'Оценка CVSS V3': cvss_v3_score
-                }
-            }
-        )
+                result[title]['severity'] = severity
+                result[title]['cvss_v3_score'] = cvss_v3_score
     return result
 
 
 if __name__ == '__main__':
     a = get_bulletins()
-    pprint(a)
+    jinja_template = Template(template)
+    EMAIL_HOST = ''
+    EMAIL_PORT = 465
+    EMAIL_HOST_PASSWORD = ''
+    EMAIL_HOST_USER = ''
+    for title in a.keys():
+        msg = EmailMessage()
+        body = jinja_template.render(title=title, a=a[title])
+        msg['Subject'] = '[НКЦКИ] + Уязвимости в +  vendor + products / '
+        msg['From'] = EMAIL_HOST_USER
+        msg['To'] = ''
+        msg.set_content(body)
+        with smtplib.SMTP(host=EMAIL_HOST, port=EMAIL_PORT) as smtp_server:
+            try:
+                smtp_server.starttls(context=ssl.SSLContext(ssl.PROTOCOL_SSLv3))
+                smtp_server.login(user=EMAIL_HOST_USER, password=EMAIL_HOST_PASSWORD)
+                smtp_server.send_message(msg)
+                print('Email sended {}'.format(msg['Subject']))
+            except Exception as e:
+                print('Error sending email. Details: {} - {}'.format(e.__class__, e))
